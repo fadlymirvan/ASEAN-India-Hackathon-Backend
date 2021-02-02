8900e038b41ea37658457eaf2040e99bb15aa10e
Fadly <fadly.m.irvan@gmail.com>
2021-02-02 16:30:48 +0700

    API to Upload Data csv, json, and txt

--- src/config.py
+++ src/config.py
@@ -10,6 +10,8 @@ class Development(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER')
+    MAX_CONTENT_LENGTH = 16 * 1024 * 1024
 
 
 class Production(object):
@@ -18,6 +20,8 @@ class Production(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER')
+    MAX_CONTENT_LENGTH = 16 * 1024 * 1024
 
 
 app_config = {
--- src/views/DataViews.py
+++ src/views/DataViews.py
@@ -1,10 +1,17 @@
 from flask import request, json, Blueprint, Response, send_file
+from werkzeug.utils import secure_filename
 from ..models.DataModels import DataModels, DataSchema
 import csv
-import io
+import os
 
 data_api = Blueprint('data_api', __name__)
 data_schema = DataSchema()
+
+ALLOWED_EXTENSIONS = {'txt', 'csv', 'json'}
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @data_api.route('/', methods=['GET'])
@@ -25,14 +32,16 @@ def create():
 
     except Exception as err:
         return Response(mimetype="application/json", response=json.dumps({
-            "message": err
+            "message": err,
+            "status": 400,
         }), status=400)
 
     data = DataModels(data)
     data.save()
 
     return Response(mimetype="application/json", response=json.dumps({
-        "message": "Created Successfully!"
+        "message": "Created Successfully!",
+        "status": 201,
     }), status=201)
 
 
@@ -41,12 +50,14 @@ def get_by_id(data_id):
     req = DataModels.get_data_by_id(data_id)
     if not req:
         return Response(mimetype="application/json", response=json.dumps({
-            "message": "Data Not Found"
+            "message": "Data Not Found",
+            "status": 404,
         }), status=404)
 
     data = data_schema.dump(req)
     return Response(mimetype="application/json", response=json.dumps({
-        "data": data
+        "data": data,
+        "status": 200,
     }), status=200)
 
 
@@ -56,7 +67,8 @@ def update(data_id):
     data = DataModels.get_data_by_id(data_id)
     if not data:
         return Response(mimetype="application/json", response=json.dumps({
-            "message": "Data Not Found"
+            "message": "Data Not Found",
+            "status": 404,
         }), status=404)
 
     try:
@@ -64,13 +76,15 @@ def update(data_id):
 
     except Exception as err:
         return Response(mimetype="application/json", response=json.dumps({
-            "message": err
+            "message": err,
+            "status": 400,
         }), status=400)
 
     data.update(data)
     data = data_schema.dump(data)
     return Response(mimetype="application/json", response=json.dumps({
         "message": "Updated Successfully",
+        "status": 200,
         "data": data
     }), status=200)
 
@@ -80,17 +94,19 @@ def delete(data_id):
     data = DataModels.get_data_by_id(data_id)
     if not data:
         return Response(mimetype="application/json", response=json.dumps({
-            "message": "Data Not Found"
+            "message": "Data Not Found",
+            "status": 404,
         }), status=404)
 
     data.delete()
 
     return Response(mimetype="application/json", response=json.dumps({
         "message": "Deleted Successfully",
+        "status": 200,
     }), status=200)
 
 
-@data_api.route('/download', methods=['GET'])
+@data_api.route('/csv/download', methods=['GET'])
 def generate_csv():
     file = 'D:/Project/ASEAN-IndiaHackathon/backend/static/csv/fishing_data.csv'
     with open(file, 'w') as csvfile:
@@ -120,3 +136,34 @@ def generate_csv():
     return send_file(file, mimetype="text/csv",
                      attachment_filename='fishing_data.csv',
                      as_attachment=True)
+
+
+@data_api.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return Response(mimetype="application/json", response=json.dumps({
+            "message": "No File Part in the Request!",
+            "status": 400,
+        }), status=400)
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return Response(mimetype="application/json", response=json.dumps({
+            "message": "No File Selected for Uploading",
+            "status": 400,
+        }), status=400)
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(os.getenv('UPLOAD_FOLDER'), filename))
+        return Response(mimetype="application/json", response=json.dumps({
+            "message": "File Successfully Uploaded",
+            "status": 201,
+        }), status=201)
+
+    else:
+        return Response(mimetype="application/json", response=json.dumps({
+            "message": "File Types are not Allowed! Allowed File type ('txt', 'csv', 'json')",
+            "status": 400,
+        }), status=400)
